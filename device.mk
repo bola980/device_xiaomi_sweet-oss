2c7e5ffc378605b566f9352dcc86bd075f165552
nazunamoe <nakashimayuki@kakao.com>
2021-07-21 20:52:25 +0900

    sweet: Move SettingsOverlay to correct path

--- device.mk
+++ device.mk
@@ -123,6 +123,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigOverlaySweet \
     DialerOverlaySweet \
     FrameworkResOverlaySweet \
+    SettingsOverlay \
     SettingsProviderOverlaySweet \
     SystemUIOverlaySweet \
     TelephonyOverlaySweet
